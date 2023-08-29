9dd0d293bb5281f69b1264f62eb3b5e98b77e339
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-08-29 18:19:06 +0000

    updateded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+import { ThemeProvider } from '@/providers/ThemeProvider'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   )
 }
