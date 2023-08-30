dc20c8fe63e0631578f10fa460d46ec4ef56426e
Ivan Tom <77198289+Tomdieu@users.noreply.github.com>
2023-08-30 19:23:26 +0000

    updated

---
+++ app/(auth)/(routes)/auth/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+type AuthLayoutProps = {
+    children:React.ReactNode
+}
+
+const AuthLayout:React.FC<AuthLayoutProps> = ({children}) => {
+    return (
+        <div className="w-full ">
+            
+        </div>
+    )
+}
+
+export default AuthLayout
